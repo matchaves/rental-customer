8b3c96ba4cd42f9954b3caa3073800cc456504d8
Your Name <newmateus@hotmail.com>
2022-01-19 23:54:00 -0300

    ajsute_path

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
-# rental-customer
-
-gradle jibDockerBuild -Ptag=100 # tag do build da imagem docker
+# Rental-rental
 
 branch com initial conf application.yaml
 
+```
+base-path /customer
+```
+
+```
+GET /customer/catalog
+busca a lista de customers
+```
+
+
+###CMD PARA PEGAR AS INFOS DO CLIENTE PELO ID
+```
+GET /customer?customer_id=X
+esse endpoint faz uma pré-alocação do registrando na tabela rental um registro com
+status "processando"
+```
+
+branch com initial conf application.yaml com jib configurado
+
+###CMD BUILD USANDO JIB PARA BUILD DOCKER IMAGE COM A TAG PASSADA VIA ARGUMENTO
+gradle jibDockerBuild -Ptag=100 # tag do build da imagem docker
+
+###CMD RODA CONTAINER SETANDO O IP DO PG E A PORTA PARA A API CONECTAR NO BANCO
+###VARIAVEIS IP_PG e PORT ESTÃO REFERENCIADAS NO application.yaml
+docker run -d --name customer --env ip_pg=192.160.XX.XX colabdevops/rental:1
+-env PORT=5436
--- src/main/java/com/dvdrental/customer/controller/CustomerController.java
+++ src/main/java/com/dvdrental/customer/controller/CustomerController.java
@@ -6,10 +6,7 @@ import com.dvdrental.customer.model.Film;
 import com.dvdrental.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,15 +19,15 @@ public class CustomerController {
     private CustomerService customerService;
 
 
-    @GetMapping()
+    @GetMapping("/catalog")
     public List<Customer> get () {
         return customerService.getCustomer();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity getFilmById(@PathVariable("id") Long id ) {
-        Optional<Customer> film = customerService.getCustomerbyId(id);
-        return  film.map(c -> ResponseEntity.ok(film))
+    @GetMapping("")
+    public ResponseEntity getFilmById(@RequestParam Long customer_id ) {
+        Optional<Customer> customer = customerService.getCustomerbyId(customer_id);
+        return  customer.map(c -> ResponseEntity.ok(customer))
                 .orElse((ResponseEntity.notFound().build()));
     }
 
